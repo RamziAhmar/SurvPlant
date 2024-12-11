b3b89b1a0c3e76f69b3114c6044098bc3ba6a7b9
RamziAhmar <ramziahmar@gmail.com>
2024-12-11 14:49:54 +0700

    initial commit

---
+++ src/scripts/api.js
@@ -0,0 +1,19 @@
+// TODO: Silakan sesuaikan BASE URL dari endpoint Anda
+const BASE_URL = 'https://survplant-api-1095910163394.asia-southeast2.run.app';
+
+const ENDPOINT = {
+  predict: `${BASE_URL}/predict`,
+};
+
+class PredictAPI {
+  static async predict(data) {
+    const response = await fetch(ENDPOINT.predict, {
+      method: 'POST',
+      body: data,
+      redirect: 'follow',
+    });
+
+    const json = await response.json();
+    return json;
+  }
+}
