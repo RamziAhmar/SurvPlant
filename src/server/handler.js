8842910501359804ba16311780221a6d6dc49312
Ahmar Ramzi Sanjaya <ramziahmar@gmail.com>
2024-12-13 11:04:50 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,6 +1,5 @@
 const predictClassification = require("../services/inferenceService");
 const crypto = require("crypto");
-const { storeData } = require("../services/storeData");
 
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
@@ -37,8 +36,6 @@ async function postPredictHandler(request, h) {
     nutrition: nutrition,
     createdAt: createdAt,
   };
-
-  //await storeData(id, data);
   
   const response = h.response({
     status: "success",
